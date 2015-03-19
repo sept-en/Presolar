8a0abf1c7a69ae7d1a83352349dae0c269b87196
Sept_en <gammerxpower@gmail.com>
2015-03-19 22:21:17 +0200

    MainWindow init.

---
+++ main_window.py
@@ -0,0 +1,38 @@
+from PyQt4 import QtGui, QtCore
+
+
+class MainWindow (QtGui.QWidget):
+    def __init__ (self, parent=None):
+        super (QtGui.QWidget, self).__init__ (parent)
+
+        self.mainGrid = QtGui.QGridLayout ()
+        #self.mainGrid.resize (800, 600)
+        
+        self.basicAnalysisBox = QtGui.QVBoxLayout()
+        
+        # first line : userInput and searchButton
+        userFirstLine = QtGui.QHBoxLayout()
+        self.userInputLine = QtGui.QLineEdit()
+        self.userInputLine.resize (200, 50)
+        searchButton = QtGui.QPushButton ("Search")
+        searchButton.clicked.connect (self.searchButtonClicked)
+
+        userFirstLine.addWidget (self.userInputLine)
+        userFirstLine.addWidget (searchButton)
+
+        # second line : box with the result of analysis
+        userSecondLine = QtGui.QHBoxLayout()
+        self.userResultBox = QtGui.QTextEdit()
+        self.userResultBox.setReadOnly (True)
+        userSecondLine.addWidget (self.userResultBox)
+
+        self.basicAnalysisBox.addLayout (userFirstLine)
+        self.basicAnalysisBox.addLayout (userSecondLine)
+        
+        self.setWindowTitle ("Presolar")
+        self.resize (800, 600)
+        self.setLayout (self.basicAnalysisBox)
+
+
+    def searchButtonClicked (self, e):
+        pass

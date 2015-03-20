65e8aaa03bc9d76fa9ef53afbdfe4344181f04be
Vlad Zagorodniy <vladzzag@gmail.com>
2015-03-20 04:04:34 +0200

    Edit analyzer....

--- src/analyzer.py
+++ src/analyzer.py
@@ -3,31 +3,45 @@ import dataset
 
 class Analyzer:
 	def __init__(self, datasetsFilename):
+		"""
+		Load datasets
+		"""
 		self.datasets = dataset.Dataset.loadDatasets(datasetsFilename)
 
 		if self.datasets is None:
 			raise Exception("Invalid datasets file or file is missing.")
 
-	def predict(self, country, city, pw=400, areaOfPanel=1, panelCount=10):
-            cityDataset = dataset.Dataset.getDataset(self.datasets, country, city)
-            energyPerYear = Analyzer.getEnergyPerYear(cityDataset.irradiance, pw, areaOfPanel)
-            energyPerHour = energyPerYear / 12 / 30 / 24
-            energyPerHour *= panelCount
+	def predict(self, country, city, pw=400, areaOfPanel=1, 
+		panelCost=250, panelCount=10, costPerHour=0.05):
+		"""
+		predict
 
-            installationCostOfPanel = 15
-            costOfPanel = (250 + installationCostOfPanel) * panelCount
-            costPerHour = 0.05
-            paybackTermMonths = Analyzer.getPaybackTerm(energyPerHour, costOfPanel, costPerHour)
-            return energyPerHour, paybackTermMonths
+		param country: [string] country...
+		param city: [string] city... yo..
+		param pw: [int] power of a solar panel
+		param areaOfPanel: [int] area.
+		param panelCost: [int] cost of a solar panel
+		param panelCount: [int] number of solar panels
+		param costPerHour: [float] cost per hour
+		"""
+        cityDataset = dataset.Dataset.getDataset(self.datasets, country, city)
+        energyPerYear = Analyzer.getEnergyPerYear(cityDataset.irradiance, pw, areaOfPanel)
+        energyPerHour = energyPerYear / 12 / 30 / 24
+        energyPerHour *= panelCount
+
+        installationCostOfPanel = 15
+        costOfPanel = (panelCost + installationCostOfPanel) * panelCount
+        paybackTermMonths = Analyzer.getPaybackTerm(energyPerHour, costOfPanel, costPerHour)
+        return energyPerHour, paybackTermMonths
 
 	@staticmethod
 	def getEnergyPerYear(irradiance, pw, area, lossesRatio=0.75):
-            # convert watts to kW
-            pw /= 1000
+        # convert watts to kW
+        pw /= 1000
             
-            r = pw #/ area
-            energyPerYear = area * r * irradiance * lossesRatio
-            return energyPerYear
+        r = pw #/ area
+        energyPerYear = area * r * irradiance * lossesRatio
+        return energyPerYear
 
 	@staticmethod
 	def getPaybackTerm(energyPerHour, costOfPanel, costPerHour):
